d17e4145060172bdecafeb31c6331875f962438c
Federico Flores <floresfederico25@gmail.com>
2021-05-18 12:48:04 -0300

    tpejemplo version 1

---
+++ tpejemplo/src/main/java/com/unla/tpejemplo/TpejemploApplication.java
@@ -0,0 +1,13 @@
+package com.unla.tpejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpejemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpejemploApplication.class, args);
+	}
+
+}
---
+++ tpejemplo/src/test/java/com/unla/tpejemplo/TpejemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.tpejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpejemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
